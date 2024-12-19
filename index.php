e6b47cf36b143d742fcbe1e00ef518990700f90f
Eric Popoca <eric.mlpopoca@gmail.com>
2024-12-19 00:50:07 -0600

    Creacion principal

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    include 'includes/header.php';
+?>
+<div class="container-fluid">
+    <div class="row">
+        <!-- Sidebar -->
+        <?php include 'menu.php'; ?>
+
+        <!-- Main Content -->
+        <div class="col-md-9 d-flex justify-content-center align-items-center" style="height: 100vh;">
+            <div class="text-center">
+                <h1>Bienvenido al sistema de cotizaciones de ISIA</h1>
+            </div>
+        </div>
+    </div>
+</div>
