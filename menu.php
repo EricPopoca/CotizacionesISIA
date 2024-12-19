30c7e267bd2c8da4b91798e071ec61ccaf4f831e
Eric Popoca <eric.mlpopoca@gmail.com>
2024-12-19 00:50:26 -0600

    Creacion menu

---
+++ menu.php
@@ -0,0 +1,17 @@
+<div class="col-md-3 bg-light p-3">
+    <h4>Menú</h4>
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Clientes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Cotizaciones</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Ajustes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Cerrar sesión</a>
+        </li>
+    </ul>
+</div>
